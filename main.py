eb6df324bd5905a34e7bee1a2ab480e0dfae60e7
mirko <mirkoweber95@gmail.com>
2023-05-30 15:42:25 +0200

    initial commit ... added .gitignore, main.py and parser.py to add first argparse arguments and define basic logic in main.py for switching RNA/DNA in PDB files and/or drop the RNA/DNA tag

---
+++ main.py
@@ -0,0 +1,14 @@
+from helper.parser import parse_arguments
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    if args.switch_rna_dna:
+        with open(args.filename) as file:
+            pass
+        # open file
+            # detect what is going on
+            # if elif else what needs to be done
+        # save file
+    if args.drop_rna_dna:
+        pass
